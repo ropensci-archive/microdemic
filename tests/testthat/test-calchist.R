eec68d044ea50a22147ac1e64029800c7ce4d435
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-04 09:26:15 -0700

    comment out calchist test for now, always getting 429

--- tests/testthat/test-calchist.R
+++ tests/testthat/test-calchist.R
@@ -1,15 +1,15 @@
 context("ma_calchist")
 
-test_that("ma_calchist works as expected", {
-  ## FIXME: add vcr back later, rate limit exceeded on this, must have changed to be very very few per time period
-  # vcr::use_cassette("ma_calchist", {
-  aa <- ma_calchist(query = "And(Composite(AA.AuN=='jaime teevan'),Y>2012)",
-     atts = c('Y', 'F.FN'))
+# test_that("ma_calchist works as expected", {
+#   ## FIXME: add vcr back later, rate limit exceeded on this, must have changed to be very very few per time period
+#   vcr::use_cassette("ma_calchist", {
+#     aa <- ma_calchist(query = "And(Composite(AA.AuN=='jaime teevan'),Y>2012)",
+#        atts = c('Y', 'F.FN'))
 
-  expect_is(aa, "list")
-  expect_named(aa, c('expr', 'num_entities', 'histograms'))
-  expect_is(aa$histograms$histogram[[1]], "data.frame")
-  expect_is(aa$histograms$histogram[[2]], "data.frame")
-  expect_named(aa$histograms$histogram[[2]], c('value', 'logprob', 'count'))
-  # })
-})
+#     expect_is(aa, "list")
+#     expect_named(aa, c('expr', 'num_entities', 'histograms'))
+#     expect_is(aa$histograms$histogram[[1]], "data.frame")
+#     expect_is(aa$histograms$histogram[[2]], "data.frame")
+#     expect_named(aa$histograms$histogram[[2]], c('value', 'logprob', 'count'))
+#   })
+# })
