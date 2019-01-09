ad746a6c57327d482fc94a040fc2f40d8203eecb
christopher crew baker <chriscrewbaker@gmail.com>
2019-01-08 20:41:20 -0500

    run tests on travis

--- tests/testthat/helper-microdemic.R
+++ tests/testthat/helper-microdemic.R
@@ -1,5 +1,10 @@
 # set up vcr
 library("vcr")
+
+# set fake API key on Travis so check_key() doesn't throw an error
+if (identical(Sys.getenv("TRAVIS"), "true")) {
+  Sys.setenv("MICROSOFT_ACADEMIC_KEY" = "fake_key")
+}
 invisible(vcr::vcr_configure(
     dir = "../fixtures",
     filter_sensitive_data = list("<<<microdemic_api_key>>>" = Sys.getenv('MICROSOFT_ACADEMIC_KEY'))
