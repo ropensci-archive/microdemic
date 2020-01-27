c5987e33b094fa985e4abe65f0e56920ead67d60
Scott Chamberlain <myrmecocystus@gmail.com>
2020-01-27 11:54:52 -0800

    fix #14 - fix internal error message parsingn to account for changes in MAS error formats

--- tests/testthat/test-error-msgs.R
+++ tests/testthat/test-error-msgs.R
@@ -4,6 +4,14 @@ test_that("Detailed error messages are provided when possible", {
   skip_on_cran()
 
   vcr::use_cassette("ma_error", {
-    expect_error(ma_search(query = "hi-there"))
+    # error: Query parsing error in string "hi-there" at position 2: Unknown
+    # function/attribute 'hi'
+    expect_error(ma_search("hi-there"))
+    # error: Offset must be an integer
+    expect_error(ma_evaluate("Y=[2010, 2012)", offset = "bad-offset"))
+    # error: Access denied due to invalid subscription key. Make sure you
+    # are subscribed to an API you are trying to call and provide the right
+    # key.
+    expect_error(ma_search("Y=[2010, 2012)", key = "bad-key"))
   }, preserve_exact_body_bytes = TRUE)
 })
